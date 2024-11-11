4b4029372ef84dc1e935f82a8b9da4275aa23c7c
SongshGeo <songshgeo@gmail.com>
2024-11-11 09:46:04 +0100

    feat(analysis): :sparkles: 增加对土壤水的单位转化分析

--- past1000/utils/units.py
+++ past1000/utils/units.py
@@ -5,7 +5,6 @@
 # GitHub   : https://github.com/SongshGeo
 # Website: https://cv.songshgeo.com/
 
-from numbers import Number
 from typing import Literal, TypeAlias
 
 import pint
@@ -19,6 +18,7 @@ setup_logger()
 
 WATER_DENSITY = 1000 * ureg("kg/m^3")
 TimeUnit: TypeAlias = Literal["second", "day", "month", "year"]
+Number: TypeAlias = int | float
 
 
 def has_unit(numeric: Number | xr.DataArray) -> bool:
@@ -30,7 +30,7 @@ def has_unit(numeric: Number | xr.DataArray) -> bool:
 
 # https://earthscience.stackexchange.com/questions/20733/fluxnet15-how-to-convert-latent-heat-flux-to-actual-evapotranspiration
 def hfls_to_evapo(
-    hfls: pint.Quantity,
+    hfls: Number | pint.Quantity,
     time: TimeUnit = "day",
 ) -> pint.Quantity:
     """将潜热通量转换为实际蒸发量。
@@ -40,7 +40,6 @@ def hfls_to_evapo(
     - E: 蒸发量 (mm/day)
     - LE: 潜热通量 (W/m²)
     - λ: 水的汽化潜热 (≈ 2.45 MJ/kg)
-    - ρw: 水的密度 (1000 kg/m³)
 
     Args:
         hfls: 潜热通量，单位为 W/m²
@@ -54,12 +53,10 @@ def hfls_to_evapo(
         hfls = hfls * ureg("W/m^2")
     # 水的汽化潜热
     lambda_water = 2.45 * ureg("MJ/kg")  # 约2.45 MJ/kg at 20°C
-    # 水的密度
-    rho_water = 1000 * ureg("kg/m^3")
     # 转换为每日总量 (W = J/s)
     daily_energy = hfls * 86400 * ureg("s/day")  # 转换为 J/(m²·day)
     # 计算蒸发量 E = LE / (ρw * λ)
-    evap = daily_energy / (rho_water * lambda_water)
+    evap = daily_energy / (WATER_DENSITY * lambda_water)
     # 转换为mm/day (1 mm = 1 kg/m²)
     unit = "mm/" + time
     if isinstance(evap, pint.Quantity):
@@ -68,9 +65,8 @@ def hfls_to_evapo(
 
 
 def pr_kg_to_mm(
-    pr: pint.Quantity,
+    pr: Number | pint.Quantity,
     time: TimeUnit = "second",
-    density: pint.Quantity = WATER_DENSITY,
 ) -> pint.Quantity:
     """将以质量为单位的降水率转换为以体积为单位的降水强度。
 
@@ -84,11 +80,21 @@ def pr_kg_to_mm(
         logger.warning("没有单位，自动添加单位kg/(m^2*s).")
         pr = pr * ureg("kg/(m^2*s)")
 
-    volume = pr / density
+    volume = pr / WATER_DENSITY
     unit = "mm/" + time
     if isinstance(volume, pint.Quantity):
         return volume.to(unit)
     return volume.pint.to(unit)
+
+
+def mrso_kg_to_mm(
+    mrso: Number | pint.Quantity,
+) -> pint.Quantity:
+    """将土壤水分转换为体积单位。"""
+    if not has_unit(mrso):
+        logger.warning("没有单位，自动添加单位kg/m^2.")
+        mrso = mrso * ureg("kg/m^2")
+    return mrso / WATER_DENSITY
 
 
 def convert_cmip_units(
@@ -103,4 +109,6 @@ def convert_cmip_units(
         data = pr_kg_to_mm(data)
     if variable == "hfls":
         data = hfls_to_evapo(data)
+    if variable == "mrso":
+        data = mrso_kg_to_mm(data)
     return data.pint.quantify().pint.to(output_units)
